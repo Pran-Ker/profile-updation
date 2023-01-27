45b2d9289b134539bff1bb9f829f40313e05d60c
Pran-Ker <hebbarpran@gmail.com>
2023-01-27 12:03:39 +0530

    Components of middle section --error free

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 2. Write the JavaScript code using React for displaying the profile, allowing edits and previewing them instantly on the side. You can add the display name and phone number to the right side bar.
 3. If the user clicks save changes, it should persist to the database via the service call.
 4. Please add a reset button to get the data back to the unmodified state (as it was before user changes).
-5. You do not have to implement the profile picture upload functionality.
+5. Try implementing the profile picture upload functionality.
 6. Build a microservice to create and update the user profile details.
-7. Use Git to create a repo and make meaningful commits so that we can understand your thought process.
-8. Deploy both the frontend page and the microservice on Netlify/Heroku or something similar
+7. Deploy both the frontend page and the microservice on Netlify/Heroku.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import uuid from 'react-uuid';
+import Main from './Main';
+import Sidebar from './Slidebar';
+
 
 function App() {
+  const [profile, setProfile] = useState(
+    localStorage.profile ? JSON.parse(localStorage.profile) : []
+  );
+  const [activeProfile, setActiveProfile] = useState(false);
+
+  useEffect(()=> {
+    localStorage.setItem("profile", JSON.stringify(profile));
+  }, [profile]);
+  
+  const onAddProfile = () => {
+    const newProfile = {
+      id: uuid(),
+      fName: "First Name",
+      lName: "Last Name",
+      dName: "Display Name",
+      email: "Email",
+      wphone: "Phone No (Work)",
+      hphone: "Phone No (Home)",
+      location: "Location"
+    };
+
+    setProfile([newProfile, ...profile]);
+    setActiveProfile(newProfile.id);
+  };
+
+  const onUpdateProfile = (updatedProfile) => {
+    const updatedProfileArr = profile.map((profile) => {
+      if (profile.id === updatedProfile.id) {
+        return updatedProfile;
+      }
+
+      return profile;
+    });
+
+    setProfile(updatedProfileArr);
+  };
+  
+  const getActiveProfile = () => {
+    return profile.find(({ id }) => id === activeProfile)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar
+        profile={profile}
+        onAddProfile={onAddProfile}
+        activeProfile={activeProfile}
+        setActiveProfile={setActiveProfile}
+      />
+      <Main activeProfile={getActiveProfile()} onUpdateProfile={onUpdateProfile} />
     </div>
   );
 }
